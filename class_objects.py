a8dd373db2cb7cccc7a90188a24165c89e38d6b5
Carly DaCosta <carly.a.dacosta@gmail.com>
2014-12-01 14:52:23 -0800

    cleaned up comments

--- class_objects.py
+++ class_objects.py
@@ -1,8 +1,8 @@
 import memcache, requests, os, seed
 
 MEMCACHE_SERVERS = os.environ.get('MEMCACHE_SERVERS')
-MEMCACHIER_PASSWORD = os.environ.get('MEMCACHIER_PASSWORD')
-MEMCACHIER_USERNAME = os.environ.get('MEMCACHIER_USERNAME')
+# MEMCACHIER_PASSWORD = os.environ.get('MEMCACHIER_PASSWORD')
+# MEMCACHIER_USERNAME = os.environ.get('MEMCACHIER_USERNAME')
 
 DATA="data"
 ITEMS="items"
@@ -17,23 +17,21 @@ class CompareVcs():
 		self.vc2 = VC(vc2_path)
 		
 	def compare_investments(self): 
-
-		#returns VC data
+		print "At compare investments"
+		# Returns VC data
 		self.vc1.get_data()
 		self.vc2.get_data()	
-
-		print "at compare investments"
-		# returns investment data
+		
+		# Returns investment data
 		i_1 = self.vc1.get_investments()
 		i_2 = self.vc2.get_investments()
 
-		#querying the db and finding the intersection of companies
+		# Finds investments in common using investment data
 		common_investments_set = (set(item['invested_in']['name'] for item in i_1[ITEMS]) & set(item['invested_in']['name'] for item in i_2[ITEMS]))
 		print "Investments in common: ", common_investments_set
 
 		common_paths_list = (set(item['invested_in']['path'] for item in i_1[ITEMS]) & set(item['invested_in']['path'] for item in i_2[ITEMS]))
-		print "common paths: ", common_paths_list
-
+		
 		for pc_path in common_paths_list:
 			pc_path = pc_path.replace("organization/", "").encode("utf8")
 			print pc_path
@@ -51,46 +49,41 @@ class VC():
 	def __init__(self, vc_path):
 		if vc_path is None:
 			raise Exception("No VC path received to instantiate VC instance.")
-		self.mc = memcache.Client([MEMCACHE_SERVERS],[MEMCACHIER_USERNAME],
-                               [MEMCACHIER_PASSWORD], debug=0)
+		self.mc = memcache.Client([MEMCACHE_SERVERS], debug=0)
 		self.vc_path = vc_path
 		self.vc_data_key = "data-%s" % vc_path
 		self.vc_investments_key = "investments-%s" % vc_path
 
 	def get_data(self):
-		print "Made it to get VC data"
+		print "Getting VC data"
 		
-		print "at get_data - path: ", self.vc_path
-
 		cache = self.mc.get(self.vc_data_key)
+		
 		if cache is not None:
-			print "Data was in cache."
+			print "VC data was in cache."
 			seed.load_investment_company(cache)
 			return cache
 
 		c = Crunchbase()
 
 		data = c.get_vc_data(self.vc_path)
-		self.mc.set(self.vc_data_key, data) # put the data into cache.
-		seed.load_investment_company(data) # put the data into the DB.
+		self.mc.set(self.vc_data_key, data)
+		seed.load_investment_company(data)
 		return data 
 
 	def get_investments(self):
-		print "Made it to get VC investments"
+		print "Getting VC investments"
 
 		cache = self.mc.get(self.vc_investments_key)
 
 		if cache is not None:
 			print "Investments were in cache."
-			#seed.load_investment_details(data) # put the data into the DB, too.
 			return cache
 
 		c = Crunchbase()
 
 		data = c.get_vc_portfolio(self.vc_path)
-		self.mc.set(self.vc_investments_key, data) # put the data into cache.
-		#seed.load_investment_details(data) # put the data into the DB.
-				
+		self.mc.set(self.vc_investments_key, data)
 		return data
 
 
@@ -100,25 +93,24 @@ class PortfolioCompany():
 	def __init__(self, pc_path):
 		if pc_path is None:
 			print "No PC path received to instantiate PC instance."
-		self.mc = memcache.Client([MEMCACHE_SERVERS],[MEMCACHIER_USERNAME],
-                               [MEMCACHIER_PASSWORD], debug=0)
+		self.mc = memcache.Client([MEMCACHE_SERVERS], debug=0)
 		self.pc_path = pc_path
 		self.pc_data_key = "data-%s" % pc_path
 		
 	def get_data(self):
-		print "Made it to PC data"
-		print "path used: ", self.pc_path
-
+		print "Getting PC data"
+		
 		cache = self.mc.get(self.pc_data_key)
 		if cache is not None:
+			print "PC data was in cache."
 			seed.load_portfolio_company(cache)
 			return cache
 
 		c = Crunchbase()
 
 		data = c.get_pc_data(self.pc_path)
-		self.mc.set(self.pc_data_key, data) # put the data into cache.
-		seed.load_portfolio_company(data) # put the data into the DB, too.
+		self.mc.set(self.pc_data_key, data)
+		seed.load_portfolio_company(data)
 		return data
 
 
@@ -126,8 +118,7 @@ class PortfolioCompany():
 def save_vc_list():
 	URL_BASE = "http://api.crunchbase.com/v/2/"
 	API_KEY = os.environ.get('CRUNCHBASE_API_KEY')
-	mc = memcache.Client([MEMCACHE_SERVERS],[MEMCACHIER_USERNAME],
-                               [MEMCACHIER_PASSWORD], debug=0)	
+	mc = memcache.Client([MEMCACHE_SERVERS], debug=0)	
 	c = Crunchbase()
 
 	data = c.get_vc_list()
@@ -162,24 +153,24 @@ class Crunchbase():
 	
 	def __init__(self):
 		self.API_KEY = os.environ.get('CRUNCHBASE_API_KEY')
-		self.mc = memcache.Client([MEMCACHE_SERVERS],[MEMCACHIER_USERNAME],
-                               [MEMCACHIER_PASSWORD], debug=0)
+		self.mc = memcache.Client([MEMCACHE_SERVERS], debug=0)
 
 	def get_vc_list(self):
 		#send request for vcs only to get the number of pages (this will include page 1 but we will just redo the request specifically by page #)
-		print "Queried Crunchbase"
+		print "Queried Crunchbase for VC list"
 		response = requests.get(self.URL_BASE + "organizations?organization_types=investor&user_key=" + self.API_KEY)
 		return response.json()[DATA]
 		
 	def get_vc_data(self, vc_path):
-		print "at crunchbase vc_data - path:", vc_path
+		print "At crunchbase vc_data - path:", vc_path
 		return self._query(vc_path, "")
 		
 	def get_vc_portfolio(self, vc_path):
+		print "At crunchbase vc_portfolio - path:", vc_path
 		return self._query(vc_path, "/investments")	
 
 	def get_pc_data(self, pc_path):
-		print "at crunchbase pc_data - path:", pc_path
+		print "At crunchbase pc_data - path:", pc_path
 		return self._query(pc_path, "")
 
 	def _query(self, vc_path, target=""):
